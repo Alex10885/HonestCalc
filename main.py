5de3e53078a58d914f15b5a3edaa614654ebb8d4
Alex10885 <aledor10885@gmail.com>
2023-01-25 11:33:15 +0300

    Added the function of saving a number in memory and calling

--- main.py
+++ main.py
@@ -19,19 +19,42 @@ msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
+memory = float(0)
+
 
 while True:
     print(msg_0)
     oper_list = ['+', '-', '*', '/']
     x, oper, y = input().split()
+    answer = ""
 
-    if x.isalpha() or y.isalpha():
-        print(msg_1)
-    elif oper not in oper_list:
+    if (x.isalpha() or y.isalpha()) and (y != 'M' or x != 'M'):
+        # print(msg_1)
+        if x == 'M':
+            x = memory
+        if y == "M":
+            y = memory
+    if oper not in oper_list:
         print(msg_2)
     else:
+
         try:
-            print(calculate(float(x), oper, float(y)))
-            break
+            result = calculate(float(x), oper, float(y))
+            print(result)
         except ZeroDivisionError:
             print(msg_3)
+            continue
+
+        print(msg_4)
+        answer = input().split()  # store result?
+        if answer[0] == ("y" or "Y"):
+            memory = result
+
+        print(msg_5)
+        answer = input().split()
+        if answer[0] == ("n" or "N"):
+            break
+        if answer[0] == ("y" or "Y"):
+            continue
